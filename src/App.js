6c965430abc7757d74f59d3d89ab3d394af83551
jackhowa <jack.howard@washpost.com>
2021-02-25 17:14:18 -0600

    Add some more instructions

--- src/App.js
+++ src/App.js
@@ -8,21 +8,26 @@ function App() {
   // import a function that says it's importable 
   // make sure that function or element 
   // https://stackoverflow.com/questions/53326353/dynamically-import-a-react-component-if-that-file-exists-otherwise-show-a-defau/53326847
-  const FileExistsChecker = React.lazy(() => import(`./${targetFileName}.js`).catch(() => ({ default: () => <div>Not Found</div> })))
+  const DynamicFileRendered = React.lazy(() => import(`./${targetFileName}.js`).catch(() => ({ default: () => <p>Not Found</p> })))
 
   return (
     <div className="App">
-      <p>Try NewYa, Ya, or Kramer. Watch the network tab for the chunks.</p>
+      <p>Type to see if the file exists in the in the app</p>
+      <p>Try NewYa, Ya, or Kramer. Watch the network tab for the chunks imported dynamically.</p>
+      <p>You will see not found if the file you're typing exists in the `src` folder.</p>
       <form>
         <label>
           Target file name:
           <input type="text" value={targetFileName} onChange={(e) => setTargetFileName(e.target.value)} name="file-name" />
         </label>
       </form>
-      {targetFileName && <p>Test if file {targetFileName} exists</p>}
+      {/* just basic label, mostly to show that it's looking for a js file */}
+      {targetFileName && <p>Test if file {targetFileName}.js exists</p>}
+
+      {/* show loading state while the file is looking up after changing the input */}
       <React.Suspense fallback={<p>Checking that file for ya...</p>}>
         {
-          <FileExistsChecker />
+          <DynamicFileRendered />
         }
       </React.Suspense>
     </div >
